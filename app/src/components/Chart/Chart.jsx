aef58650029b78d0d85ed3f1846af8470362ec3a
Justin Russell <Justin@justins-macbook.lan>
2020-07-03 12:04:10 -0700

    adding charts

--- app/src/components/Chart/Chart.jsx
+++ app/src/components/Chart/Chart.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Charts = () => {
+
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+      const fetchAPI = async () => {
+          setDailyData(await fetchDailyData());
+      }
+
+      fetchAPI();
+  })
+
+  const lineChart = (
+
+  )
+
   return(
     <h1>Charts</h1>
   ) 
